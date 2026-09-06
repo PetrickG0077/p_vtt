23d6ac54ae275368f7d63c367439e184ee0b3a0e
PetrickG0077 <petrickgamer28@gmail.com>
2026-09-05 22:42:33 -0300

    perf: limit asset image dimensions and file size

--- src/main/java/com/petrick/vtt/feature/asset/thumbnail/AssetThumbnailLoader.java
+++ src/main/java/com/petrick/vtt/feature/asset/thumbnail/AssetThumbnailLoader.java
@@ -28,11 +28,11 @@ import java.util.Iterator;
  */
 public final class AssetThumbnailLoader {
 
-    private static final long MAX_FILE_SIZE_BYTES = 256L * 1024L * 1024L;
+    private static final long MAX_FILE_SIZE_BYTES = 64L * 1024L * 1024L;
 
-    private static final int MAX_IMAGE_WIDTH = 16000;
+    private static final int MAX_IMAGE_WIDTH = 5120;
 
-    private static final int MAX_IMAGE_HEIGHT = 16000;
+    private static final int MAX_IMAGE_HEIGHT = 5120;
 
     private static final int ANIMATED_FALLBACK_PLACEHOLDER_SIZE = 64;
 
